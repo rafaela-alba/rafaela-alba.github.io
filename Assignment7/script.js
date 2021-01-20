46079b6d653eb1972b2d498709084095ce122b98
rafaela-alba <77078907+rafaela-alba@users.noreply.github.com>
2021-01-19 21:17:05 -0500

    Add files via upload

---
+++ Assignment7/script.js
@@ -0,0 +1,24 @@
+let massLucas = 78;
+let heightLucas = 1.69;
+let lucasBMI = massLucas / heightLucas ** 2;
+
+let massJohn = 92; //85 kg
+let heightJohn = 1.95; //1.76 m
+let johnBMI = massJohn / heightJohn ** 2;
+
+let lucasHigherBMI = (lucasBMI > johnBMI);
+
+if (lucasHigherBMI === true) {
+    console.log("Lucas has a higher BMI than John.")
+} else {
+    console.log("Lucas does not have a higher BMI than John.")
+}
+
+massLucas = 95;
+heightLucas = 1.88;
+
+massJohn = 85;
+heightJohn = 1.76;
+
+console.log("Lucas and John are friends, Lucas is " + heightLucas + " m tall and John is " + heightJohn + " m tall.");
+console.log("So john has a higher BMI than Lucas, that is " + johnBMI + ".")
