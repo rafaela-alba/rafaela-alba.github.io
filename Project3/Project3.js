708bc05ec0be503171f950447d38f6d2fe84d8f6
rafaela-alba <77078907+rafaela-alba@users.noreply.github.com>
2021-01-25 11:58:30 -0500

    Add files via upload

---
+++ Project3/Project3.js
@@ -0,0 +1,59 @@
+let secretNumber = 60; //Math.trunc(Math.random() * 100) + 1;
+
+const enterGuess = document.querySelector('.enterGuess'); //button
+const guessBox = document.querySelector('.guessBox'); //textbox
+const message = document.querySelector('.instruct');
+const reset = document.querySelector('.reset');
+let score = 10;
+let highScore = 0;
+
+enterGuess.addEventListener('click', check);
+
+function check() {
+
+    let guess = Number(guessBox.value); //user guess
+
+    if (score === 1) {
+        message.textContent = 'Game Over.';
+        document.querySelector('.mysteryBox').textContent = `${secretNumber}`;
+        enterGuess.style.display = 'none';
+        document.querySelector('body').style.background = `linear-gradient(to top right, #300909, #db0303)`;
+    }
+    else if (isNaN(guess)) {
+        message.textContent = 'Please only enter a number';
+    }
+    else if (guess < 1 || guess > 100) {
+        message.textContent = 'Please enter a number between 1 and 100';
+    }
+    else if (guess === secretNumber) {
+        message.textContent = 'You won the game!';
+        document.querySelector('body').style.background = 'linear-gradient(to top right, #2b7cf7, #47e777)';
+        enterGuess.style.display = 'none';
+        document.querySelector('.mysteryBox').textContent = `${secretNumber}`;
+        if (score >= highScore) { highScore = score; };
+        document.querySelector('.highScore').textContent = `Highscore: ${highScore}`;
+
+    }
+    else if (guess > secretNumber) {
+        message.textContent = 'Too high!';
+
+    }
+    else {
+        message.textContent = 'Too low';
+
+    }
+
+    if (guess !== secretNumber) {
+        score--;
+    };
+
+    document.querySelector('.score').textContent = `Score: ${score}`;
+};
+
+reset.addEventListener('click', playAgain);
+
+function playAgain() {
+    score = 10;
+
+}
+
